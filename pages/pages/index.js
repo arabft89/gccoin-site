ff2b701681d085d3afcd3be9a179ac493b0019fd
arabft89 <faisal.t.arab89@gmail.com>
2025-07-30 08:19:07 +0300

    Delete pages/pages directory

--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to GCCoin 🌍</h1>
-      <p>This bilingual site is under construction and will host the full whitepaper, roadmap, and token info soon.</p>
-    </main>
-  );
-}
