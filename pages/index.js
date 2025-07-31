6d2c5d3da7bc54886d2a5fd93d98e88db26bf449
arabft89 <faisal.t.arab89@gmail.com>
2025-07-31 10:06:30 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ export default function Home() {
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Welcome to GCCoin 🌍</h1>
       <p>This bilingual site includes roadmap, whitepaper, and tokenomics details.</p>
-      <a href="/downloads/GCCoin_Whitepaper_v1_EN_FULL.docx" download>Download Whitepaper (EN)</a><br/>
-      <a href="/downloads/GCCoin_Whitepaper_v1_AR_FULL.docx" download>Download Whitepaper (AR)</a>
+      <a href="/GCCoin_Whitepaper_v1_EN_FULL.docx" download>Download Whitepaper (EN)</a><br/>
+      <a href="/GCCoin_Whitepaper_v1_AR_FULL.docx" download>Download Whitepaper (AR)</a>
     </main>
   );
 }
