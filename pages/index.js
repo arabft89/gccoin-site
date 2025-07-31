77d49f16df75a6ff6d496cef51beb740c6bc5606
arabft89 <faisal.t.arab89@gmail.com>
2025-07-31 10:12:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to GCCoin 🌍</h1>
-      <p>This bilingual site includes roadmap, whitepaper, and tokenomics details.</p>
+      <h1>GCCoin - The Digital Asset of the Gulf 🌍</h1>
+      <p>Built to unify the financial future of the GCC region with secure, Sharia-compliant blockchain utility.</p>
       <a href="/GCCoin_Whitepaper_v1_EN_FULL.docx" download>Download Whitepaper (EN)</a><br/>
       <a href="/GCCoin_Whitepaper_v1_AR_FULL.docx" download>Download Whitepaper (AR)</a>
     </main>
