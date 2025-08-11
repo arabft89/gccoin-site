956524d425e094674b3e4409fa548e410bc6d214
arabft89 <faisal.t.arab89@gmail.com>
2025-08-11 08:58:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@lib"] = require("path").resolve(__dirname, "lib");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
