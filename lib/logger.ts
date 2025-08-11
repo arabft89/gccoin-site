6e8bac448e886e9906bbc4a3ff151c39477461e6
arabft89 <faisal.t.arab89@gmail.com>
2025-08-11 08:38:48 +0300

    Create logger.ts

---
+++ lib/logger.ts
@@ -0,0 +1,20 @@
+// lib/logger.ts
+
+const isProd = process.env.NODE_ENV === "production";
+
+export const log = (...args: any[]) => {
+  if (!isProd) {
+    console.log(...args);
+  }
+};
+
+export const warn = (...args: any[]) => {
+  if (!isProd) {
+    console.warn(...args);
+  }
+};
+
+export const error = (...args: any[]) => {
+  // Always log errors, even in production
+  console.error(...args);
+};
